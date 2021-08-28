31e7f821d969bb31f4b5725b2bd4a403a5bde6b3
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 22:16:12 +0530

    Added: code to find singlet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,29 @@
-#!/bin/bash -x 
-flip=$(($((RANDOM%10))%2))
+#!/bin/bash -x
 
-if [ $flip -eq 1 ]
-then
-	echo "Heads"
-else
-	echo "Tails"
-fi
+read -p "Enter the number of flip you want:" num
+headsCount=0
+tailsCount=0
 
+for (( i=1; i<=$num; i++ ))
+do	
+	flip=$(($((RANDOM%10))%2))
+	if [ $flip -eq 0 ]
+	then
+		echo "Heads"
+		headsCount=$(($headsCount+1))
+	else
+		echo "Tails"
+		tailsCount=$(($tailsCount+1))
+	fi
+done
+echo "numbers of head is:"$headsCount
+echo "number of tail is::" $tailsCount
+
+percentageOfHeads=$(($(($headsCount*100))/$num))
+percentageOfTails=$(($(($tailsCount*100))/$num))
+
+echo "Heads=$percentageOfHeads%"
+echo "Tails=$percentageOfTails%"
+
+dict[singlet]="{Heads=$headsCount:percentage=$percentageOfHeadsTails=$tailsCount:percentage=$percentageOfTails}"
+echo ${dict[@]}
