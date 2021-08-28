49bb7859dada1e4dd54896aeffe11edeea9510ba
Dipti104706 <diptimayeebehura104@gmail.com>
2021-08-28 23:13:21 +0530

    Added: code to find triplet combination

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,46 +1,76 @@
 #!/bin/bash -x
-
 read -p "Enter the number of flip you want:" num
-HHcount=0
-TTcount=0
-HTcount=0
-THcount=0
-
+HHHcount=0
+TTTcount=0
+HHTcount=0
+THHcount=0
+TTHcount=0
+HTTcount=0
+HTHcount=0
+THTcount=0
 for (( i=1; i<=$num; i++ ))
 do	
-	flip=$(($((RANDOM%10))%4))
+	flip=$(($((RANDOM%10))%8))
 	if [ $flip -eq 0 ]
 	then
-		echo "HH"
-		HHcount=$(($HHcount+1))
+		echo "HHH"
+		HHHcount=$(($HHHcount+1))
 	elif [ $flip -eq 1 ]
 	then
-		echo "TT"
-		TTcount=$(($TTcount+1))
+		echo "TTT"
+		TTTcount=$(($TTTcount+1))
 	elif [ $flip -eq 2 ]
 	then
-		echo "HT"
-		HTcount=$(($HTcount+1))
+		echo "HHT"
+		HHTcount=$(($HHTcount+1))
+	elif [ $flip -eq 3 ]
+	then
+		echo "THH"
+		THHcount=$(($THHcount+1))
+	elif [ $flip -eq 4 ]
+	then
+		echo "TTH"
+		TTHcount=$(($TTHcount+1))
+	elif [ $flip -eq 5 ]
+	then		echo "HTT"
+		HTTcount=$(($HTTcount+1))
+	elif [ $flip -eq 6 ]
+	then
+		echo "HTH"
+		HTHcount=$(($HTHcount+1))
 	else
-		echo "TH"
-		THcount=$(($THcount+1))
+		echo "THT"
+		THTcount=$(($THTcount+1))
 	fi
 done
-echo "number of HH is:"$HHcount
-echo "number of TT is::"$TTcount
-echo "number of HT is::"$HTcount
-echo "number of TH is::"$THcount
 
-percentageOfHH=$(($(($HHcount*100))/$num))
-percentageOfTT=$(($(($TTcount*100))/$num))
-percentageOfHT=$(($(($HTcount*100))/$num))
-percentageOfTH=$(($(($THcount*100))/$num))
+echo "number of HHH is::"$HHHcount
+echo "number of TTT is::"$TTTcount
+echo "number of HHT is::"$HHTcount
+echo "number of THH is::"$THHcount
+echo "number of TTH is::"$TTHcount
+echo "number of HTT is::"$HTTcount
+echo "number of HTH is::"$HTHcount
+echo "number of THT is::"$THTcount
+ 
+percentageOfHHH=$(($(($HHHcount*100))/$num))
+percentageOfTTT=$(($(($TTTcount*100))/$num))		
+percentageOfHHT=$(($(($HHTcount*100))/$num))		
+percentageOfTHH=$(($(($THHcount*100))/$num))		
+percentageOfTTH=$(($(($TTHcount*100))/$num))		
+percentageOfHTT=$(($(($HTTcount*100))/$num))		
+percentageOfHTH=$(($(($HTHcount*100))/$num))		
+percentageOfTHT=$(($(($THTcount*100))/$num)) 		
 
-echo "HH=$percentageOfHH%"
-echo "TT=$percentageOfTT%"
-echo "HT=$percentageOfHT%"
-echo "TH=$percentageOfTH%"
+echo "HHH=$percentageOfHHH%"		
+echo "TTT=$percentageOfTTT%"		
+echo "HHT=$percentageOfHHT%"		
+echo "THH=$percentageOfTHH%"		
+echo "TTH=$percentageOfTTH%"		
+echo "HTT=$percentageOfHTT%"		
+echo "HTH=$percentageOfHTH%"		
+echo "THT=$percentageOfTHT%" 		
 
-dict[doublet]="{HH=$HHcount:%=$percentageOfHH TT=$TTcount:%=$percentageOfTT HT=$HTcount:%=$percentageOfHT TH=$THcount:%=$percentageOfTH}"
+dict[triplet]="{HHH=$HHHcount:%=$percentageOfHHH TTT=$TTTcount:%=$percentageOfTTT HHT=$HHTcount:%=$percentageOfHHT THH=$THHcount:%=$percentageOfTHH TTH=$TTHcount:%=$percentageOfTTH HTT=$HTTcount:%=$percentageOfHTT HTH=$HTHcount:%=$percentageOfHTH THT=$THTcount:%=$percentageOfTHT }"		
 
 echo ${dict[@]}
